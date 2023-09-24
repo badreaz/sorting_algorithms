b8ab755d41255d59814e2e59654c54c70b3e13ac
badreaz <baz.23.2019@gmail.com>
2023-09-24 15:18:58 +0400

    check size and array before sorting

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,6 +11,8 @@ void shell_sort(int *array, size_t size)
 	size_t i, j, gap = 0;
 	int tmp;
 
+	if (!array || size < 2)
+		return;
 	while (gap < size / 3)
 		gap = 3 * gap + 1;
 	for (; gap >= 1; gap /= 3)
