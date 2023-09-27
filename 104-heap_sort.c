00853eecb881fe6b24ebbdde37bd9c2b4ee7ef54
badreaz <baz.23.2019@gmail.com>
2023-09-27 13:16:46 +0400

    add sift_down function

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,5 +1,9 @@
 #include "sort.h"
 
+void sift_down(int *array, size_t start, size_t end, size_t size);
+
+
+
 /**
  * heap_sort - sorts an array of integers using
  * the heap algorithm.
@@ -8,32 +12,54 @@
  */
 void heap_sort(int *array, size_t size)
 {
-	size_t i, max, left, right;
+	size_t i;
 	int tmp;
 
-	for (i = size / 2; i > 0; i--)
-	{
-		max = i - 1;
-		left = 2 * (i - 1) + 1;
-		right = 2 * (i - 1) + 2;
-		if (left < size && array[left] > array[max])
-			max = left;
-		if (right < size && array[right] > array[max])
-			max = right;
-		if (max != i - 1)
-		{
-			tmp = array[i - 1];
-			array[i - 1] = array[max];
-			array[max] = tmp;
-			print_array(array, size);
-		}
-	}
+	if (!array || size < 2)
+		return;
 
-	for (i = size - 1; i > 0; i--)
+	for (i = size - 2; (int)i >= 0; i--)
+		sift_down(array, i, size -1, size);
+
+	for (i = size - 1; i > 0;)
 	{
 		tmp = array[0];
 		array[0] = array[i];
 		array[i] = tmp;
 		print_array(array, size);
+		i--;
+		sift_down(array, 0, i, size);
+	}
+}
+
+/**
+ * sift_down - repair the heap whose root element is at index 'start'
+ * @array: array to sort.
+ * @start: start index.
+ * @end: end index.
+ * @size: array size.
+ */
+void sift_down(int *array, size_t start, size_t end, size_t size)
+{
+	size_t root, child, swap;
+	int tmp;
+
+	root = start;
+	child = 2 * root + 1;
+	while (child <= end)
+	{
+		swap = root;
+		if (array[swap] < array[child])
+			swap = child;
+		if (child + 1 <= end && array[swap] < array[child + 1])
+			swap = child + 1;
+		if (swap == root)
+			return;
+		tmp = array[root];
+		array[root] = array[swap];
+		array[swap] = tmp;
+		root = swap;
+		print_array(array, size);
+		child = 2 * root + 1;
 	}
 }
