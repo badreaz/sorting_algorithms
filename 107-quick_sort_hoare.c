44a998cf44c020f484ad98b21189165fb087f9e0
badreaz <baz.23.2019@gmail.com>
2023-09-29 23:02:21 +0400

    create 107-quick_sort_hoare.c

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * quick_sort_hoare - sort an array of integer using
+ * quick sort algorithm.
+ * @array: array of int.
+ * @size: array size.
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+}
